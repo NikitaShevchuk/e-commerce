d61508e47e3f736778ad0ef224df812fa9847a1b
Nikita Shevhcuk <shevchuk.nikita.gh@gmail.com>
2023-03-04 08:57:23 +0200

    Edit links in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,10 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="icon" href="/favicon.svg" />
+                <link rel="icon" href="/e-commerce/favicon.svg" />
                 <meta name="theme-color" content="#ffffff" />
                 <meta name="description" content="Web site created using Next.js" />
-                <link rel="manifest" href="/manifest.json" />
+                <link rel="manifest" href="/e-commerce/manifest.json" />
             </Head>
             <body>
                 <Main />
